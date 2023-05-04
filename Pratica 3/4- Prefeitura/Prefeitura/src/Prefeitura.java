c605929aab218ff181d87a898cc0bf6fbae88dd6
Bnunes216 <106674371+Bnunes216@users.noreply.github.com>
2023-05-04 19:16:24 -0300

    Add files via upload

---
+++ Pratica 3/4- Prefeitura/Prefeitura/src/Prefeitura.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+public class Prefeitura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StringBuilder mensagem = new StringBuilder();
+        
+        int media, n1, n2, n3, i=0, matricula;
+        
+    do{
+ i++;
+ matricula = Integer.parseInt(JOptionPane.showInputDialog("Insira a matricula do aluno de numero: " + i));       
+ 
+ if(matricula>=0){
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota  1 do aluno de numero: " + i));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota 2 do aluno de numero: " + i));        
+        n3 = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota 3 do aluno de numero: " + i));            
+       media = (n1+n2+n3)/3;     
+
+       if(media >= 70){
+       mensagem.append("APROVADO");   
+       JOptionPane.showMessageDialog(null,mensagem);
+       }else if(media>=60 &&media <70){
+       mensagem.append("ESTA EM RECUPERAÇAO");   
+       JOptionPane.showMessageDialog(null,mensagem);       
+       }else if(media<60){
+       mensagem.append("REPROVADO");   
+       JOptionPane.showMessageDialog(null,mensagem);  
+       }
+}    
+ 
+ 
+ 
+    }while(matricula>=0);    
+    System.exit(0);
+    }
+    
+}
